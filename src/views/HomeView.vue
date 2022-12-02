5a164a250cc479f092be8e311b831a419c0c065c
Allan Wright <allan.s.wright@gmail.com>
2022-12-02 08:56:33 +0000

    Start work on landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <RouterLink to="/game">Play</RouterLink>
   </main>
 </template>
+
+<style scoped>
+a {
+  background-color: var(--colour-primary-back);
+  border-radius: 5px;
+  color: var(--colour-primary-text);
+  display: block;
+  font-size: x-large;
+  line-height: 60px;
+  text-align: center;
+  width: 150px;
+}
+</style>

204c7d01b147ece380ea46b6d51aacbc5b3c14c4
Allan Wright <allan.s.wright@gmail.com>
2022-12-10 07:56:07 +0000

    Implement adding new players

--- src/components/AddPlayer.vue
+++ src/components/AddPlayer.vue
@@ -1,11 +1,41 @@
-<script setup lang="ts">
-import IconPlus from "@/components/icons/IconPlus.vue";
-</script>
-
 <template>
   <div>
     <div>Add Player</div>
-    <input type="text" title="Name" placeholder="Name" />
-    <IconPlus />
+    <input type="text" title="Name" placeholder="Name" v-model="name" />
+    <IconPlus @click="addPlayer()" />
   </div>
 </template>
+
+<script lang="ts">
+import IconPlus from "@/components/icons/IconPlus.vue";
+import { usePlayerStore } from "@/stores/players";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    const playerStore = usePlayerStore();
+
+    const testMe = (name: string) => {
+      playerStore.addPlayer(name);
+    };
+
+    return {
+      playerStore,
+      testMe,
+    };
+  },
+  components: {
+    IconPlus,
+  },
+  data() {
+    return {
+      name: "" as string,
+    };
+  },
+  methods: {
+    addPlayer: function () {
+      this.testMe(this.name);
+    },
+  },
+});
+</script>
--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -1,6 +1,22 @@
 <template>
   <ul>
-    <li>Allan</li>
-    <li>Owen</li>
+    <li v-for="player in players" v-bind:key="player">{{ player }}</li>
   </ul>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { usePlayerStore } from "@/stores/players";
+
+export default defineComponent({
+  setup() {
+    const playerStore = usePlayerStore();
+
+    const players = playerStore.players;
+
+    return {
+      players,
+    };
+  },
+});
+</script>

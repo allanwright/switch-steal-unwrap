d7725afc477edcd3c95efb1f8fb797583bf0a2e7
Allan Wright <allan.s.wright@gmail.com>
2022-12-11 07:52:12 +0000

    Rewrite actions

--- src/stores/game.ts
+++ src/stores/game.ts
@@ -22,22 +22,20 @@ class Player {
 
 export const useGameStore = defineStore("gameStore", {
   state: () => ({
-    // TODO: The wording of these actions is pretty average, make them better
-    // TODO: Actions 6 and 8 are a gender disaster, fix them
     /* eslint-disable prettier/prettier */
     actions: [
       new Action(1, "Make somebody else unwrap their gift"),
-      new Action(2, "Exchange with anyone or keep your gift"),
-      new Action(3, "Trade gifts with anyone wearing red"),
-      new Action(4, "Trade gifts with the person on your right"),
+      new Action(2, "Keep your gift or swap it with someone else of your choice"),
+      new Action(3, "Swap your gift with someone wearing red"),
+      new Action(4, "Swap your gift with the person on your right"),
       new Action(5, "Everyone pass their gift once to the right"),
-      new Action(6, "Guys only - pass their gift once to the left"),
+      new Action(6, "If you were born on an even year, pass your gift once to the left"),
       new Action(7, "Unwrap your gift, if unwrapped roll again"),
-      new Action(8, "Girls only - pass their gift to the right"),
+      new Action(8, "If you were born on an odd year, pass your gift once to the right"),
       new Action(9, "Everyone pass their gift once to the left"),
-      new Action(10, "Everyone pass across the room"),
-      new Action(11, "Trade gifts with anyone wearing green"),
-      new Action(12, "Trade gifts with the person on your left"),
+      new Action(10, "Everyone swap gifts across the room"),
+      new Action(11, "Swap your gift with someone wearing green"),
+      new Action(12, "Swap your gift with the person on your left"),
     ] as Action[],
     players: [] as Player[],
     playerCounter: 0 as number,

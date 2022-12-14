2fd1647de3d340339478d76195e15b039a1494ae
Allan Wright <allan.s.wright@gmail.com>
2022-12-14 05:45:17 +0000

    Start sytling landing page

--- src/components/AddPlayer.vue
+++ src/components/AddPlayer.vue
@@ -12,9 +12,33 @@ const name = ref("");
 </script>
 
 <template>
-  <div>
-    <div>Add Player</div>
+  <div class="group">
     <input type="text" title="Name" placeholder="Name" v-model="name" />
-    <IconPlus @click="addPlayer()" />
+    <div class="button" @click="addPlayer()">Add Player</div>
   </div>
 </template>
+
+<style scoped>
+.group {
+  display: flex;
+  height: 35px;
+}
+
+input {
+  border-radius: 10px 0 0 10px;
+  border-style: none;
+  text-indent: 10px;
+}
+
+input:focus {
+  outline-width: 0;
+}
+
+.button {
+  background-color: var(--colour-3);
+  border-radius: 0 10px 10px 0;
+  display: inline-block;
+  line-height: 35px;
+  padding: 0 10px 0 10px;
+}
+</style>
